a973781413c59ec2629e42087470ce0e151dda02
Jared <shixuran@qq.com>
2022-06-21 16:52:15 +0800

    more methods of string type

---
+++ chapter-1/stirng-2.js
@@ -0,0 +1,14 @@
+// 常见的 String 方法
+
+let email = 'makima@gmail.com'
+
+// let result = email.lastIndexOf('m')
+
+// let result = email.slice(7, 12)
+
+// 不建议使用stirng.substr()
+// let result = email.substring(7, 12)
+
+// let result = email.replace('i', '1')
+
+console.log(result)
