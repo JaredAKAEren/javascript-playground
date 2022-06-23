00272786faeb282cf7b1532289f2e38b755f174d
Jared <shixuran@qq.com>
2022-06-23 18:43:41 +0800

    numbers in javascript chapter-1

---
+++ chapter-1/number-1.js
@@ -0,0 +1,22 @@
+// numbers
+
+// let radius = 10
+// const pi = 3.14
+
+// let result = pi * radius ** 2
+
+// console.log(result)
+
+let likes = 10
+
+likes++
+likes += 1
+likes += 10
+
+console.log(likes)
+
+// NaN means not a number
+
+let result = 'This tweet has ' + likes + ' likes'
+
+console.log(result)
