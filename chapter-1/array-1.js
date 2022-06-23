36fe8767c72e50ee9b8dc550d0b63eda528b9e71
Jared <shixuran@qq.com>
2022-06-23 19:37:42 +0800

    array in javascript

---
+++ chapter-1/array-1.js
@@ -0,0 +1,14 @@
+let evils = ['pochita', 'makima', 'denji']
+
+evils[2] = 'reze'
+
+// console.log(evils.length)
+
+// let result = evils.join(',')
+// let result = evils.indexOf('reze')
+// let result = evils.concat(['denji', 'power'])
+let result = evils.push('power')
+result = evils.pop()
+
+console.log(evils)
+console.log(result)
