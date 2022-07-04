a409a28a36efb0e0317c4937dc24affe873cfba5
Jared <shixuran@qq.com>
2022-07-04 14:16:37 +0800

    try different comparison in javascript

---
+++ chapter-1/compare.js
@@ -0,0 +1,13 @@
+let age = 23
+
+// 弱比较（不同类型的数据可以相等）
+// console.log(age == 23)
+// console.log(age == '23')
+// console.log(age != 23)
+// console.log(age != '23')
+
+// 强比较（不同类型的数据不可以相等）
+console.log(age === 23)
+console.log(age === '23')
+console.log(age !== 23)
+console.log(age !== '23')
