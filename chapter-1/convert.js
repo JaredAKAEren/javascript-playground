9f037619948bf1f54105159c5c2870fca7a816fa
Jared <shixuran@qq.com>
2022-07-04 14:29:24 +0800

    try to convert the type of values in javascript

---
+++ chapter-1/convert.js
@@ -0,0 +1,16 @@
+let score = '100'
+
+console.log(score + 1)
+
+score = Number(score)
+console.log(score + 1)
+console.log(typeof score)
+
+// let result = Number('makima')
+// let result = String(50)
+let result = Boolean(100)
+result = Boolean(0)
+result = Boolean('0')
+result = Boolean('')
+
+console.log(result, typeof result)
