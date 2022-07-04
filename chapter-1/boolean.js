8604eaf295696948569b93ed8bc3caee2880f05f
Jared <shixuran@qq.com>
2022-07-04 14:02:25 +0800

    try some boolean type in javascript

--- chapter-1/boolean.js
+++ chapter-1/boolean.js
@@ -1,1 +1,23 @@
 console.log(true, false)
+
+let email = 'eren@gmail.com'
+let names = ['makima', 'pochita', 'danji']
+
+// let result = email.includes('@')
+// let result = names.includes('makima')
+
+// console.log(result)
+
+let age = 23
+
+console.log(age == 23)
+console.log(age == 30)
+console.log(age != 30)
+console.log(age > 30)
+console.log(age >= 23)
+
+let myName = 'Eren'
+
+console.log(myName == 'Eren')
+console.log(myName == 'eren')
+console.log(myName > 'Aren')
