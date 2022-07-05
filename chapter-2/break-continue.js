3e1ab5335c0d32be9521b5f88e96fe16640b3203
Jared <shixuran@qq.com>
2022-07-05 19:35:37 +0800

    try break and continue in java script

---
+++ chapter-2/break-continue.js
@@ -0,0 +1,16 @@
+const scores = [50, 25, 0, 30, 100, 20, 10]
+
+for (let i = 0; i < scores.length; i++) {
+    const score = scores[i];
+
+    if (score === 0) {
+        continue
+    }
+
+    console.log('your score: ', score)
+
+    if (score === 100) {
+        console.log('congrats, you got the top score.')
+        break
+    }
+}
