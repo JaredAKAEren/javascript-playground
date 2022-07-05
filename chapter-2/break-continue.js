d6519a4c2ce711b4467daf43fc1ce597dc67ce71
Jared <shixuran@qq.com>
2022-07-05 19:48:53 +0800

    try switch statement in javascript

--- chapter-2/break-continue.js
+++ chapter-2/break-continue.js
@@ -1,7 +1,7 @@
 const scores = [50, 25, 0, 30, 100, 20, 10]
 
 for (let i = 0; i < scores.length; i++) {
-    const score = scores[i];
+    const score = scores[i]
 
     if (score === 0) {
         continue
---
+++ chapter-2/switch.js
@@ -0,0 +1,22 @@
+const grade = 'A'
+
+switch (grade) {
+    case 'A':
+        console.log('your got an A!')
+        break
+    case 'B':
+        console.log('your got a B!')
+        break
+    case 'C':
+        console.log('your got a C!')
+        break
+    case 'D':
+        console.log('your got a D!')
+        break
+    case 'E':
+        console.log('your got a E!')
+        break
+    default:
+        console.log('not a valid grade.')
+        break
+}
