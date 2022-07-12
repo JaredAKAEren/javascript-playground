4df4736a99f27962a63065a1f3b82f9ab1040b12
Jared <shixuran@qq.com>
2022-07-12 17:45:59 +0800

    try to create a method in object

---
+++ chapter-4/object-2.js
@@ -0,0 +1,12 @@
+let user = {
+    name: 'makima',
+    age: 23,
+    email: 'makima@gmail.com',
+    location: 'China',
+    blogs: ['Chainsaw man', 'Look back'],
+    login: () => console.log('the user is logged in'),
+    logout: () => console.log('the user is logged out')
+}
+
+user.login()
+user.logout()
