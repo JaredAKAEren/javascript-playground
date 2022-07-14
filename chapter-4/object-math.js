66ff67b8cedcc896c26c85ddf3c08686a83699de
Jared <shixuran@qq.com>
2022-07-14 19:43:36 +0800

    try Math object in javascript

---
+++ chapter-4/object-math.js
@@ -0,0 +1,19 @@
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.E)
+
+const area = 7.7
+
+// 四舍五入
+console.log(Math.round(area))
+// 向下取整
+console.log(Math.floor(area))
+// 向上取整
+console.log(Math.ceil(area))
+// 截取整数部分
+console.log(Math.trunc(area))
+
+const random = Math.random()
+
+console.log(random)
+console.log(Math.round(random * 100))
